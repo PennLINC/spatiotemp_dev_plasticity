7e0d2b7fb83f1b40af6e5fbe1dbd7881caa6e7ac
Valerie Sydnor <valeriejill.sydnor@gmail.com>
2022-03-15 16:51:02 -0400

    add gam.estimate.smooth function

--- gam_models/GAM_functions.R
+++ gam_models/GAM_functions.R
@@ -149,6 +149,51 @@ gam.smooth.predict <- function(measure, atlas, dataset, region, smooth_var, cova
   return(smooth.fit)
 }
 
+#SMOOTH ESTIMATES FUNCTION
+##Function to estimate the zero-averaged gam smooth fit 
+gam.estimate.smooth <- function(measure, atlas, dataset, region, smooth_var, covariates, knots, set_fx = FALSE, increments){
+  
+  #Fit the gam
+  dataname <- sprintf("%s.%s.%s", measure, atlas, dataset) 
+  gam.data <- get(dataname)
+  parcel <- region
+  region <- str_replace(region, "-", ".")
+  modelformula <- as.formula(sprintf("%s ~ s(%s, k = %s, fx = %s) + %s", region, smooth_var, knots, set_fx, covariates))
+  gam.model <- gam(modelformula, method = "REML", data = gam.data)
+  gam.results <- summary(gam.model)
+  
+  #Extract gam input data
+  df <- gam.model$model #extract the data used to build the gam, i.e., a df of y + predictor values 
+  
+  #Create a prediction data frame
+  np <- increments #number of predictions to make; predict at np increments of smooth_var
+  thisPred <- data.frame(init = rep(0,np)) #initiate a prediction df 
+  
+  theseVars <- attr(gam.model$terms,"term.labels") #gam model predictors (smooth_var + covariates)
+  varClasses <- attr(gam.model$terms,"dataClasses") #classes of the model predictors and y measure
+  thisResp <- as.character(gam.model$terms[[2]]) #the measure to predict
+  for (v in c(1:length(theseVars))) { #fill the prediction df with data for predictions. These data will be used to predict the output measure (y) at np increments of the smooth_var, holding other model terms constant
+    thisVar <- theseVars[[v]]
+    thisClass <- varClasses[thisVar]
+    if (thisVar == smooth_var) { 
+      thisPred[,smooth_var] = seq(min(df[,smooth_var],na.rm = T),max(df[,smooth_var],na.rm = T), length.out = np) #generate a range of np data points, from minimum of smooth term to maximum of smooth term
+    } else {
+      switch (thisClass,
+              "numeric" = {thisPred[,thisVar] = median(df[,thisVar])}, #make predictions based on median value
+              "factor" = {thisPred[,thisVar] = levels(df[,thisVar])[[1]]}, #make predictions based on first level of factor 
+              "ordered" = {thisPred[,thisVar] = levels(df[,thisVar])[[1]]} #make predictions based on first level of ordinal variable
+      )
+    }
+  }
+  pred <- thisPred %>% select(-init)
+  
+  #Estimate the smooth trajectory 
+  estimated.smooth <- smooth_estimates(object = gam.model, data = pred)
+  estimated.smooth <- estimated.smooth %>% select(age, est)
+  
+  return(estimated.smooth)
+}
+
 #POSTERIOR DISTRIBUTION SMOOTHS FUNCTION
 ##Function to simulate the posterior distribution from a fitted GAM, calculate smooths for individual posterior draws, and return smooth max and min values + 95% credible intervals
 gam.posterior.smooths <- function(measure, atlas, dataset, region, smooth_var, covariates, knots, set_fx = FALSE, draws, increments, return_draws = TRUE){
